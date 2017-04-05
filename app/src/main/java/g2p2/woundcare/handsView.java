e2ec70cbe272d7d604bdf079790bffbee64f6fc1
nklfrost <niklasklinelange@gmail.com>
2017-04-05 13:42:10 +0200

    layout stuff

---
+++ app/src/main/java/g2p2/woundcare/handsView.java
@@ -0,0 +1,31 @@
+package g2p2.woundcare;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Niklas on 4/5/2017.
+ */
+
+public class handsView extends View {
+    Bitmap test;
+
+    public handsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        test = BitmapFactory.decodeResource(getResources(),R.drawable.handstemp);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        Paint myStyle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        //myStyle.setColor(0xff101010);
+        myStyle.setColor(Color.rgb(65,176,176));
+        //canvas.drawBitmap(test,0,0,myStyle);
+    }
+}
