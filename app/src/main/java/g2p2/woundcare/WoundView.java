7876e3fb0ed102e38fb32424cf2d722f5b8fb2d4
nklfrost <niklasklinelange@gmail.com>
2017-04-14 10:53:55 +0200

    layout stuff

--- app/src/main/java/g2p2/woundcare/WoundView.java
+++ app/src/main/java/g2p2/woundcare/WoundView.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Xfermode;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,8 +20,9 @@ import java.util.ArrayList;
  */
 
 public class WoundView extends View {
-    Bitmap wound;
-    Paint coolStyle;
+    Bitmap wound, woundAplha, woundClean, woundClean2;
+
+    Paint coolStyle,masked;
     float X,Y,startX,startY,stopX,stopY,currentX,currentY,paintX,paintY;
     ArrayList<Float> paintXs = new ArrayList<Float>();
     ArrayList<Float> paintYs = new ArrayList<Float>();
@@ -27,18 +31,46 @@ public class WoundView extends View {
     public WoundView(Context c, AttributeSet as){
         super(c,as);
         wound = BitmapFactory.decodeResource(getResources(),R.drawable.maxresdefault);
+        woundClean = BitmapFactory.decodeResource(getResources(),R.drawable.maxresdefault_clean);
+        woundClean = BitmapFactory.decodeResource(getResources(),R.drawable.maxresdefault_clean);
+        //woundAplha = BitmapFactory.;
         coolStyle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        masked = new Paint(Paint.ANTI_ALIAS_FLAG);
         X=0;
         Y=0;
-        coolStyle.setColor(Color.RED);
+        coolStyle.setColor(Color.BLACK);
+        masked.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        //masked.setColor(Color.TRANSPARENT);
     }
 
     @Override
     protected void onDraw(Canvas c){
+
+        bitMaker();
+
         c.drawBitmap(wound,X,Y,coolStyle); //draws the wound
+
         for(int i=0;i<paintXs.size();i++){
-            c.drawCircle(paintXs.get(i),paintYs.get(i),50,coolStyle);
+            //c.drawCircle(paintXs.get(i)+X,paintYs.get(i)+Y,50,coolStyle);
         }
+        c.drawBitmap(woundAplha,X,Y,masked);
+        //c.drawBitmap(woundClean,X,Y,masked);
+
+    }
+
+    void bitMaker(){
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        woundAplha = Bitmap.createBitmap(wound.getWidth(), wound.getHeight(), conf); // this creates a MUTABLE bitmap
+        Canvas canvas = new Canvas(woundAplha);
+        //canvas.drawBitmap(wound,0,0,coolStyle);
+        for(int i=0;i<paintXs.size();i++){
+            canvas.drawCircle(paintXs.get(i),paintYs.get(i),50,coolStyle);
+            if (i>1){
+                //canvas.drawp make a path from point to point
+            }
+        }
+        canvas.drawBitmap(woundClean,0,0,masked);
+        //masked.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
     }
 
     @Override
@@ -63,12 +95,18 @@ public class WoundView extends View {
             this.invalidate(); //refreshes the view ("this" view).
         }
         else if(handsView.what==1){
+
+
+
+
             if (paintX!=e.getX() | paintY!=e.getY()){
-                paintXs.add(e.getX());
-                paintYs.add(e.getY());
+                paintXs.add(e.getX()-X);
+                paintYs.add(e.getY()-Y);
             }
             paintX=e.getX();
             paintY=e.getY();
+
+
             this.invalidate(); //refreshes the view ("this" view).
         }
         return true;
