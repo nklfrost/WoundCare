c4828625d696cb9644d77d250cd7033659f32167
loghinfrancisc <loghinfrancisc@gmail.com>
2017-05-15 17:19:03 +0200

    app_update

--- app/src/main/java/g2p2/woundcare/CreditsActivity.java
+++ app/src/main/java/g2p2/woundcare/CreditsActivity.java
@@ -1,5 +1,7 @@
 package g2p2.woundcare;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,5 +17,22 @@ public class CreditsActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_credits);
+
+
     }
+
+    private void showInternetConenctionAlert(String alert, String title, final String uriString) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage(alert);
+        alertDialog.setTitle(title);
+        alertDialog.setCancelable(true);
+        alertDialog.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        alertDialog.show();
+    }
+
 }
